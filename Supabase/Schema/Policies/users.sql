803325334163bd6d7a06311590a560d05e518428
hablaplay-dev <hablaplay@gmail.com>
2025-10-05 14:14:37 -0400

    Update users.sql

--- Supabase/Schema/Policies/users.sql
+++ Supabase/Schema/Policies/users.sql
@@ -1,1 +1,4 @@
 ALTER TABLE public.users ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "Users can insert own data" ON public.users FOR INSERT TO public WITH CHECK ((auth.uid() = user_id));
+CREATE POLICY "Users can update own data" ON public.users FOR UPDATE TO public USING ((auth.uid() = user_id));
+CREATE POLICY "Users can view own data" ON public.users FOR SELECT TO public USING ((auth.uid() = user_id));
