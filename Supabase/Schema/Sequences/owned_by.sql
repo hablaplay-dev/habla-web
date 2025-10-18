6bfffd9d04abc516c49712b2676e50257be4edab
hablaplay-dev <hablaplay@gmail.com>
2025-10-18 18:55:41 -0400

    Create owned_by.sql

---
+++ Supabase/Schema/Sequences/owned_by.sql
@@ -0,0 +1,6 @@
+ALTER SEQUENCE matches_id_seq OWNED BY matches.id;
+ALTER SEQUENCE tickets_id_seq OWNED BY tickets.id;
+ALTER SEQUENCE ticket_answers_id_seq OWNED BY ticket_answers.id;
+ALTER SEQUENCE email_outbox_id_seq OWNED BY email_outbox.id;
+ALTER SEQUENCE onboarding_submissions_id_seq OWNED BY onboarding_submissions.id;
+ALTER SEQUENCE seed_audit_id_seq OWNED BY seed_audit.id;
